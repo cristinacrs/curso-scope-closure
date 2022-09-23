0b66919db5fc3d916057d64285cb4358175e90e7
Cristina Cruz Cortés <cristinacruzcortes@gmail.com>
2022-09-23 13:32:45 -0500

    function scope

---
+++ source/scope/function.js
@@ -0,0 +1,12 @@
+function greeting(){
+    let userName = 'Cris';
+    console.log(userName);
+
+    if(userName == 'Cris'){
+        console.log(`Hello ${userName}`);
+    }
+}
+
+greeting();
+
+//console.log(userName); No se puede acceder a la variable
