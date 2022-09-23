a3e1d1948b7d1c4b388d9d022a8b0ff3c841096b
Cristina Cruz Cortés <cristinacruzcortes@gmail.com>
2022-09-23 17:43:04 -0500

    hoisting

---
+++ source/hoisting/index.js
@@ -0,0 +1,14 @@
+
+// var nameOfDog //undefined
+// console.log(nameOfDog);
+// var nameOfDog = 'Freyis';
+// console.log(nameOfDog);
+
+var perrito; //undefined
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${perrito}`);
+}
+
+var perrito = 'Keisi';
